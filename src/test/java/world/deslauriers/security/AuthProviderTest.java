c1e33bd19b49baefcbf435db35b619c9c9e712c0
tdeslauriers <thomas.deslauriers@gmail.com>
2021-07-02 07:13:33 -0500

    added testing for auth provider impl

--- src/test/java/world/deslauriers/security/AuthProviderTest.java
+++ src/test/java/world/deslauriers/security/AuthProviderTest.java
@@ -1,16 +1,19 @@
 package world.deslauriers.security;
 
+import com.nimbusds.jwt.JWTParser;
+import com.nimbusds.jwt.SignedJWT;
 import io.micronaut.http.*;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.security.authentication.UsernamePasswordCredentials;
 import io.micronaut.security.token.jwt.render.AccessRefreshToken;
+import io.micronaut.security.token.jwt.render.BearerAccessRefreshToken;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @MicronautTest
 public class AuthProviderTest {
@@ -21,6 +24,8 @@ public class AuthProviderTest {
 
     private static String VALID_USERNAME = "darth.vader@empire.com";
     private static String VALID_PASSWORD = "NowThisIsPod-Racing!!!";
+    private static String USERROLE_1 = "sith";
+    private static String USERROLE_2 = "starpilot";
 
     @Test
     void testLoginSuccess(){
@@ -30,10 +35,26 @@ public class AuthProviderTest {
                 .accept(MediaType.APPLICATION_JSON_TYPE)
                 .body(new UsernamePasswordCredentials(VALID_USERNAME, VALID_PASSWORD));
 
-        HttpResponse<AccessRefreshToken> response = client
+        HttpResponse<BearerAccessRefreshToken> response = client
                 .toBlocking()
-                .exchange(request, AccessRefreshToken.class);
+                .exchange(request, BearerAccessRefreshToken.class);
 
         assertEquals(HttpStatus.OK, response.getStatus());
+        assertNotNull(response.body());
+        assertNotNull(response.body().getAccessToken());
+        assertEquals(VALID_USERNAME, response.body().getUsername());
+        var role1 = response.body().getRoles()
+                .stream()
+                .filter(s -> s.equals(USERROLE_1))
+                .findFirst();
+        assertTrue(role1.isPresent());
+        assertEquals(role1.get(), USERROLE_1);
+
+        var role2 = response.body().getRoles()
+                .stream()
+                .filter(s -> s.equals(USERROLE_2))
+                .findFirst();
+        assertTrue(role2.isPresent());
+        assertEquals(role2.get(), USERROLE_2);
     }
 }
